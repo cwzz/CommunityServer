35988227f4fae33a7780cfc996bfb0443c05cb15
zp2946062178 <2799712178@qq.com>
2019-06-23 23:46:55 +0800

    解决查询文章列表的bug

--- src/main/java/com/nju/edu/community/dao/PostDao.java
+++ src/main/java/com/nju/edu/community/dao/PostDao.java
@@ -19,13 +19,13 @@ public interface PostDao extends JpaRepository<Post,String> {
     @Query(value = "select p from Post p where p.title like %?1%")
     ArrayList<Post> searchArticle(String keywords);
 
-    @Query(value = "select p from Post p where p.category=:category and p.postTag=:tag and p.state='Published' order by p.visits desc")
+    @Query(value = "select p from Post p where p.category=:category and p.postTag=:tag and p.state=1 order by p.visits desc")
     ArrayList<Post> searchArticleByTag(@Param("category")String category, @Param("tag")String tag);
 
-    @Query(value = "select p from Post p where p.category=:category and p.state='Published' order by p.visits desc " )
+    @Query(value = "select p from Post p where p.category=:category and p.state=1 order by p.visits desc " )
     ArrayList<Post> searchArticleByCategory(@Param("category")String category);
 
-    @Query(value = "select p from Post p where p.state='Published' order by p.visits desc ")
+    @Query(value = "select p from Post p where p.state=1 order by p.visits desc ")
     ArrayList<Post> searchAllArticle();
 
     @Query(value = "update Post p set p.interestNum=p.interestNum+1 where p.pid=:id")
